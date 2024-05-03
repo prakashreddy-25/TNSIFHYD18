f9d47456a2a0df2cb101d9c49fe7c7b1a0b7edc2
prakashreddy-25 <168462818+prakashreddy-25@users.noreply.github.com>
2024-05-03 21:37:08 +0530

    Add files via upload

---
+++ BItwiseOp.java
@@ -0,0 +1,15 @@
+package com.si.cse;
+
+public class BItwiseOp {
+	public static void main(String[] args) {
+		int a=10,b=12;
+		System.out.println("bitwise AND operation "+(a&b));
+		System.out.println("bitwise OR operation "+(a|b));
+		System.out.println("bitwise EXOR operation "+(a^b));
+		System.out.println("bitwise COMPLIMENT  operation "+(~a));
+		System.out.println("bitwise LEFT SHIFT operation "+(a<<b));
+		System.out.println("bitwise RIGHT SHIFT operation "+(a>>b));
+		
+	}
+
+}
